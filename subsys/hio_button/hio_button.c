36d996b05cadcda4fb2b2b8afc45a1586a9b51d7
hubmartin <hub.martin@gmail.com>
2025-03-11 17:52:27 +0100

    hio_button: remove EXT button

--- subsys/hio_button/hio_button.c
+++ subsys/hio_button/hio_button.c
@@ -24,7 +24,6 @@
 LOG_MODULE_REGISTER(hio_button, CONFIG_HIO_BUTTON_LOG_LEVEL);
 
 static const struct gpio_dt_spec m_button_int = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
-static const struct gpio_dt_spec m_button_ext = GPIO_DT_SPEC_GET(DT_ALIAS(sw1), gpios);
 
 static int read_button(enum hio_button_channel channel, bool *is_pressed)
 {
@@ -33,10 +32,6 @@ static int read_button(enum hio_button_channel channel, bool *is_pressed)
 	switch (channel) {
 	case HIO_BUTTON_CHANNEL_INT:
 		ret = gpio_pin_get_dt(&m_button_int);
-		break;
-
-	case HIO_BUTTON_CHANNEL_EXT:
-		ret = gpio_pin_get_dt(&m_button_ext);
 		break;
 
 	default:
@@ -62,9 +57,6 @@ static int cmd_button_read(const struct shell *shell, size_t argc, char **argv)
 
 	if (strcmp(argv[1], "int") == 0) {
 		channel = HIO_BUTTON_CHANNEL_INT;
-
-	} else if (strcmp(argv[1], "ext") == 0) {
-		channel = HIO_BUTTON_CHANNEL_EXT;
 
 	} else {
 		shell_error(shell, "invalid channel name");
@@ -104,7 +96,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 	sub_button,
 
 	SHELL_CMD_ARG(read, NULL,
-	              "Read button state (format int|ext).",
+	              "Read button state (format int).",
 	              cmd_button_read, 2, 0),
 
 	SHELL_SUBCMD_SET_END
@@ -118,7 +110,6 @@ SHELL_CMD_REGISTER(button, &sub_button, "Button commands.", print_help);
 #define MIN_PRESS_LENGTH 1000
 
 static struct hio_edge m_edge_int;
-static struct hio_edge m_edge_ext;
 
 struct button_data {
 	int64_t last_event;
@@ -132,14 +123,13 @@ struct button_data {
 };
 
 static struct button_data m_button_data_int = {0};
-static struct button_data m_button_data_ext = {0};
 
 static hio_button_event_cb m_event_cb = NULL;
 static void *m_user_data = NULL;
 
 static enum hio_button_channel button_data_to_channel(struct button_data *data)
 {
-	return data == &m_button_data_int ? HIO_BUTTON_CHANNEL_INT : HIO_BUTTON_CHANNEL_EXT;
+	return HIO_BUTTON_CHANNEL_INT;
 }
 
 static void click_breakup_work_handler(struct k_work *work)
@@ -179,7 +169,7 @@ static void edge_event_cb(struct hio_edge *edge, enum hio_edge_event event, void
 		return;
 	}
 
-	struct button_data *data = edge == &m_edge_int ? &m_button_data_int : &m_button_data_ext;
+	struct button_data *data = &m_button_data_int;
 
 	int64_t uptime = k_uptime_get();
 	int64_t diff = uptime - data->last_event;
@@ -210,12 +200,6 @@ int hio_button_set_event_cb(hio_button_event_cb cb, void *user_data)
 		return ret;
 	}
 
-	ret = hio_edge_watch(&m_edge_ext);
-	if (ret) {
-		LOG_ERR("Call `hio_edge_watch` (BUTTON_EXT) failed: %d", ret);
-		return ret;
-	}
-
 	return 0;
 }
 
@@ -230,20 +214,9 @@ static int init(void)
 		return -EINVAL;
 	}
 
-	if (!device_is_ready(m_button_ext.port)) {
-		LOG_ERR("Device `BUTTON_EXT` not ready");
-		return -EINVAL;
-	}
-
 	ret = gpio_pin_configure_dt(&m_button_int, GPIO_INPUT);
 	if (ret) {
 		LOG_ERR("Call `gpio_pin_configure_dt` (BUTTON_INT) failed: %d", ret);
-		return ret;
-	}
-
-	ret = gpio_pin_configure_dt(&m_button_ext, GPIO_INPUT);
-	if (ret) {
-		LOG_ERR("Call `gpio_pin_configure_dt` (BUTTON_EXT) failed: %d", ret);
 		return ret;
 	}
 
@@ -251,10 +224,6 @@ static int init(void)
 	k_work_init(&m_button_data_int.click_work, click_work_handler);
 	k_work_init(&m_button_data_int.hold_work, hold_work_handler);
 	k_timer_init(&m_button_data_int.breakup_timer, click_breakup_timer_handler, NULL);
-	k_work_init(&m_button_data_ext.click_breakup_work, click_breakup_work_handler);
-	k_work_init(&m_button_data_ext.click_work, click_work_handler);
-	k_work_init(&m_button_data_ext.hold_work, hold_work_handler);
-	k_timer_init(&m_button_data_ext.breakup_timer, click_breakup_timer_handler, NULL);
 
 	ret = hio_edge_init(&m_edge_int, &m_button_int, false);
 	if (ret) {
@@ -283,36 +252,6 @@ static int init(void)
 	ret = hio_edge_set_inactive_duration(&m_edge_int, 10);
 	if (ret) {
 		LOG_ERR("Call `hio_edge_set_inactive_duration` (BUTTON_INT) failed: %d", ret);
-		return ret;
-	}
-
-	ret = hio_edge_init(&m_edge_ext, &m_button_ext, false);
-	if (ret) {
-		LOG_ERR("Call `hio_edge_init` (BUTTON_EXT) failed: %d", ret);
-		return ret;
-	}
-
-	ret = hio_edge_set_callback(&m_edge_ext, edge_event_cb, NULL);
-	if (ret) {
-		LOG_ERR("Call `hio_edge_set_callback` (BUTTON_EXT) failed: %d", ret);
-		return ret;
-	}
-
-	ret = hio_edge_set_cooldown_time(&m_edge_ext, 10);
-	if (ret) {
-		LOG_ERR("Call `hio_edge_set_cooldown_time` (BUTTON_EXT) failed: %d", ret);
-		return ret;
-	}
-
-	ret = hio_edge_set_active_duration(&m_edge_ext, 10);
-	if (ret) {
-		LOG_ERR("Call `hio_edge_set_active_duration` (BUTTON_EXT) failed: %d", ret);
-		return ret;
-	}
-
-	ret = hio_edge_set_inactive_duration(&m_edge_ext, 10);
-	if (ret) {
-		LOG_ERR("Call `hio_edge_set_inactive_duration` (BUTTON_EXT) failed: %d", ret);
 		return ret;
 	}
 
